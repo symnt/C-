708fcd345ff1f81acff2f1872b0413524ac86a24
Şeymanur Erşahin <52773744+symnt@users.noreply.github.com>
2019-07-11 21:40:32 +0300

    Add files via upload

---
+++ lab_at_yarisi/lab_at_yarisi/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace lab_at_yarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_baslat = new System.Windows.Forms.Button();
+            this.btn_sıfırla = new System.Windows.Forms.Button();
+            this.txt_durum = new System.Windows.Forms.TextBox();
+            this.pic_1 = new System.Windows.Forms.PictureBox();
+            this.pic_2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.BackColor = System.Drawing.Color.FloralWhite;
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_1.Location = new System.Drawing.Point(126, 127);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(565, 13);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = "---------------------------------------------------------------------------------" +
+    "--------------------------------------------------------------------------------" +
+    "-------------------------\r\n";
+            this.lbl_1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(697, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(9, 351);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı\r\nı";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(657, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 27);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Bitiş Çizgisi";
+            // 
+            // btn_baslat
+            // 
+            this.btn_baslat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_baslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_baslat.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_baslat.Location = new System.Drawing.Point(438, 15);
+            this.btn_baslat.Name = "btn_baslat";
+            this.btn_baslat.Size = new System.Drawing.Size(75, 28);
+            this.btn_baslat.TabIndex = 10;
+            this.btn_baslat.Text = "Başla";
+            this.btn_baslat.UseVisualStyleBackColor = true;
+            this.btn_baslat.Click += new System.EventHandler(this.btn_baslat_Click);
+            // 
+            // btn_sıfırla
+            // 
+            this.btn_sıfırla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_sıfırla.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_sıfırla.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_sıfırla.Location = new System.Drawing.Point(519, 15);
+            this.btn_sıfırla.Name = "btn_sıfırla";
+            this.btn_sıfırla.Size = new System.Drawing.Size(75, 28);
+            this.btn_sıfırla.TabIndex = 11;
+            this.btn_sıfırla.Text = "Sıfırla";
+            this.btn_sıfırla.UseVisualStyleBackColor = true;
+            this.btn_sıfırla.Click += new System.EventHandler(this.btn_sıfırla_Click);
+            // 
+            // txt_durum
+            // 
+            this.txt_durum.Enabled = false;
+            this.txt_durum.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_durum.Location = new System.Drawing.Point(15, 21);
+            this.txt_durum.Name = "txt_durum";
+            this.txt_durum.Size = new System.Drawing.Size(395, 32);
+            this.txt_durum.TabIndex = 12;
+            // 
+            // pic_1
+            // 
+            this.pic_1.ErrorImage = null;
+            this.pic_1.Image = global::lab_at_yarisi.Properties.Resources.d1876c_a4bb8d65b6ea4e7e90290e3e031702e8;
+            this.pic_1.Location = new System.Drawing.Point(12, 63);
+            this.pic_1.Name = "pic_1";
+            this.pic_1.Size = new System.Drawing.Size(79, 57);
+            this.pic_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_1.TabIndex = 7;
+            this.pic_1.TabStop = false;
+            // 
+            // pic_2
+            // 
+            this.pic_2.ErrorImage = null;
+            this.pic_2.Image = global::lab_at_yarisi.Properties.Resources.hrs;
+            this.pic_2.Location = new System.Drawing.Point(12, 210);
+            this.pic_2.Name = "pic_2";
+            this.pic_2.Size = new System.Drawing.Size(79, 57);
+            this.pic_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_2.TabIndex = 15;
+            this.pic_2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.ErrorImage = null;
+            this.pic3.Image = global::lab_at_yarisi.Properties.Resources.EquatorialKnobbyAustraliancattledog_size_restricted;
+            this.pic3.Location = new System.Drawing.Point(12, 323);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(79, 57);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 16;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.BackColor = System.Drawing.Color.FloralWhite;
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_2.Location = new System.Drawing.Point(117, 272);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(565, 13);
+            this.lbl_2.TabIndex = 17;
+            this.lbl_2.Text = "---------------------------------------------------------------------------------" +
+    "--------------------------------------------------------------------------------" +
+    "-------------------------\r\n";
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.BackColor = System.Drawing.Color.FloralWhite;
+            this.lbl_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_3.Location = new System.Drawing.Point(126, 387);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(565, 13);
+            this.lbl_3.TabIndex = 18;
+            this.lbl_3.Text = "---------------------------------------------------------------------------------" +
+    "--------------------------------------------------------------------------------" +
+    "-------------------------\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "ATILGAN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "UÇAN YELE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "FIRTINA";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(712, 344);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(62, 83);
+            this.panel1.TabIndex = 22;
+            this.panel1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(847, 485);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic_2);
+            this.Controls.Add(this.txt_durum);
+            this.Controls.Add(this.btn_sıfırla);
+            this.Controls.Add(this.btn_baslat);
+            this.Controls.Add(this.pic_1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pic_1;
+        private System.Windows.Forms.Button btn_baslat;
+        private System.Windows.Forms.Button btn_sıfırla;
+        private System.Windows.Forms.TextBox txt_durum;
+        private System.Windows.Forms.PictureBox pic_2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ lab_at_yarisi/lab_at_yarisi/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_at_yarisi
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_baslat_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //bir kontrolun left ve top özelliklerine atama yapabilirsiniz.Ancak right ve bottom özelliklerinin o anki değerini alabilirsiniz.
+
+            
+            int sayı1 = rnd.Next(1, 15);
+            int sayı2 = rnd.Next(1, 15);
+            int sayı3 = rnd.Next(1, 15);
+            pic_1.Left += sayı1;
+            pic_2.Left += sayı2;
+            pic3.Left += sayı3;
+            
+            
+            if (pic_1.Right > label4.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("ATILGAN YARIŞI KAZANDI!!");
+            }
+            else if (pic_2.Right > label4.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("UÇAN YELE YARIŞI KAZANDI!!");
+            }
+            else if (pic3.Right > label4.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("FIRTINA YARIŞI KAZANDI!!");
+            }
+            else
+            {
+                txt_durum.Text = "Berabere";
+            }
+
+            if (pic_1.Right > pic_2.Right && pic_1.Right > pic3.Right)
+            {
+                txt_durum.Text = "ATILGAN yarışıönde götürüyor.";
+            }
+            else if (pic_2.Right > pic_1.Right && pic_2.Right > pic3.Right)
+            {
+                txt_durum.Text = "UÇAN YELE yarışıönde götürüyor.";
+            }
+            else if (pic3.Right > pic_1.Right && pic3.Right > pic_2.Right)
+            {
+                txt_durum.Text = "FIRTINA  yarışıönde götürüyor.";
+            }
+            
+                
+
+            
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_sıfırla_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pic_1.Left = 0;
+            pic_2.Left = 0;
+            pic3.Left = 0;
+            txt_durum.Text = "";
+
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            pic3.Left += 15;
+        }
+    }
+}
